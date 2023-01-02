f0d1140d3d379f0cbc8b7e2081274d5f4d8b24d3
sumin0012 <92019691+sumin0012@users.noreply.github.com>
2023-01-02 21:15:27 +0900

    Add files via upload

---
+++ OnMouseDown_SwitchScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // 씬 전환에 필요 
+
+// 터치하면 씬을 전환한다 
+public class OnMouseDown_SwitchScene : MonoBehaviour
+{
+    private AudioSource audio;
+    public AudioClip ClickSound;
+    public string sceneName;  // 씬 이름：Inspector에 지정
+
+    // Use this for initialization
+    void Start()
+    {
+        this.audio = this.gameObject.AddComponent<AudioSource>();
+        this.audio.clip = this.ClickSound;
+        this.audio.loop = false;
+
+    }
+    
+
+    void OnMouseDown() 
+	{ // 터치하면
+        // 씬을 전환한다 
+        SceneManager.LoadScene (sceneName);
+        this.audio.Play();
+    }
+}
